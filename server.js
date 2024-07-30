6cf82735e6debf9cf18a47f16c06f6596e1c4318
soulxmark <mark2899estimada@outlook.ph>
2024-07-30 12:48:13 +0800

    changes

--- server.js
+++ server.js
@@ -5,14 +5,25 @@
 /* ***********************
  * Require Statements
  *************************/
+const expressLayouts = require("express-ejs-layouts")
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Routes
  *************************/
+app.use(require("./routes/static"))
+//Index routes
+app.get("/",function(req,res){
+  res.render("index",{title:"Home"})
+})
 app.use(static)
 
 /* ***********************
